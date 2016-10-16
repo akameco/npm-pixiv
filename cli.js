4fcb3f25336c4cd2bfc730ac100fa5b86327e748
akameco <akameco.t@gmail.com>
2016-10-17 00:28:45 +0900

    Update

--- cli.js
+++ cli.js
@@ -1,5 +1,79 @@
 #!/usr/bin/env node
 'use strict';
+const got = require('got');
+const cheerio = require('cheerio');
+const termImg = require('term-img');
+const spawn = require('execa').spawn;
+const co = require('co');
+
+function wait() {
+	return new Promise(resolve => {
+		setTimeout(() => {
+			resolve();
+		}, 5000);
+	});
+}
+
+function step(urls) {
+	co(function * () {
+		for (const url of urls) {
+			yield fetchImage(url).then(fetchImageBuffer).then(termImg);
+			yield wait();
+		}
+	});
+}
+
+function fetchImageBuffer(url) {
+	return got(url, {
+		encoding: null,
+		headers: {
+			Referer: 'http://www.pixiv.net/'
+		}
+	}).then(res => res.body);
+}
+
+function fetchImage(url) {
+	return got(url).then(res => {
+		const $ = cheerio.load(res.body);
+		return $('.img-container img').attr('src');
+	});
+}
+
+function fetchRanking() {
+	return got('http://www.pixiv.net/ranking.php?mode=daily').then(res => {
+		const $ = cheerio.load(res.body);
+		return $('.ranking-image-item a').map((i, v) => {
+			const href = $(v).attr('href');
+			return `http://www.pixiv.net/${href}`;
+		}).get();
+	});
+}
+
+function isInstall(args) {
+	return args[0] === 'i' || args[0] === 'install';
+}
+
+function npm(args) {
+	return spawn('npm', args, {
+		cwd: process.cwd(),
+		stdio: isInstall(args) ? 'ignore' : 'inherit'
+	});
+}
+
+function displayImages() {
+	fetchRanking().then(step).catch(console.log);
+}
+
+const run = args => {
+	const ps = npm(args);
+	ps.on('exit', code => {
+		process.exit(code);
+	});
+
+	if (isInstall(args)) {
+		displayImages();
+	}
+};
 
 const args = process.argv.slice(2);
-require('.')(args);
+run(args);
--- test.js
+++
@@ -1,6 +0,0 @@
-import test from 'ava';
-import m from '.';
-
-test(async t => {
-	t.is(typeof m, 'function');
-});
